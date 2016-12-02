4c4e91690b6be623f739b077cb16f01e36260444
Maxime Martineau <maxime@prafiny.org>
2016-12-02 18:50:08 +0100

    added posts func

--- model/post.php
+++ model/post.php
@@ -1,114 +1,125 @@
 <?php
-namespace Modules\Post;
+namespace Model\Post;
 /**
- * Post model
+ * Post
  *
- * This file contains every db processings regarding the users
+ * This file contains every db action regarding the posts
  */
 
 /**
- * Get a user in db
- * @param id the id of the user in db
- * @return an object containing the attributes of the user or false if error
- * @warning this function does not yield the password
+ * Get a post in db
+ * @param id the id of the post in db
+ * @return an object containing the attributes of the post or false if error
+ * @warning the author attribute is a user object
+ * @warning the date attribute is a DateTime object
  */
 function get($id) {
     return (object) array(
         "id" => 1337,
-        "username" => "yrlgtm",
-        "name" => "User 1",
-        "email" => "yrlgtm@gmail.com",
-        "avatar" => "" 
+        "post_content" => "Text",
+        "date" => new DateTime('2011-01-01T15:03:01'),
+        "author" => Model\User\get(2)
     );
 }
 
 /**
- * Create a user in db
- * @param username the user's username
- * @param name the user's name
- * @param password the user's password
- * @param email the user's email
- * @return the id which was assigned to the created user
- * @warning this function enciphers the password
+ * Get a post with its likes, responses, the hashtags used and the post it was the response of
+ * @param id the id of the post in db
+ * @return an object containing the attributes of the post or false if error
+ * @warning the author attribute is a user object
+ * @warning the date attribute is a DateTime object
+ * @warning the likes attribute is an array of users objects
+ * @warning the hashtags attribute is an of hashtags objects
+ * @warning the responds_to attribute is either null (if the post is not a response) or a post object
  */
-function create($username, $name, $password, $email, $avatar) {
+function get_with_joins($id) {
+    return (object) array(
+        "id" => 1337,
+        "post_content" => "Ima writing a post !",
+        "date" => new DateTime('2011-01-01T15:03:01'),
+        "author" => Model\User\get(2),
+        "likes" => [],
+        "hashtags" => [],
+        "responds_to" => null
+    );
+}
+ 
+/**
+ * Create a post in db
+ * @param author_id the author user's id
+ * @param name the user's name
+ * @param mentioned_authors the array of ids of users who are mentioned in the post
+ * @param response_to the id of the post which the creating post responds to
+ * @return the id which was assigned to the created post
+ * @warning this function computes the date
+ * @warning this function extracts the mentioned users
+ * @warning this function extracts the used hashtags
+ */
+function create($author_id, $text, $response_to=null) {
     return 1337;
 }
 
 /**
- * Modify a user in db
- * @param uid the user's id to modify
- * @param username the user's username
- * @param name the user's name
- * @param email the user's email
- * @return true if everything went fine, false else
- */
-function modify($uid, $username, $name, $email) {
-    return false;
-}
-
-/**
- * Change a user's avatar
- * @param avatar_path the temporary path to the new avatar
- * @return true if everything went fine, false else
- */
-function change_avatar($uid, $avatar_path) {
-    return false;
-}
-
-/**
- * Modify a user in db
- * @param uid the user's id to modify
- * @param new_password the new password
- * @return true if everything went fine, false else
- * @warning this function enciphers the password
- */
-function change_password($uid, $new_password) {
-    return false;
-}
-
-/**
- * Delete a user in db
- * @param id the id of the user to delete
- * @return true if the user has been correctly deleted, false else
+ * Delete a post in db
+ * @param id the id of the post to delete
+ * @return true if the post has been correctly deleted, false else
  */
 function destroy($id) {
     return false;
 }
 
 /**
- * Search a user
- * @param string the string to search in the name or username
- * @return an array of the objects of find objects
- * @warning this function does not return the passwords
+ * Search for posts
+ * @param string the string to search in the text
+ * @return an array of find objects
  */
 function search($string) {
-    return [(object) array(
-        "id" => 1337,
-        "username" => "yrlgtm",
-        "name" => "User 1",
-        "email" => "yrlgtm@gmail.com"
-    )];
+    return [get(1)];
 }
 
 /**
- * Verify the user authentification
- * @param username
- * @param password
- * @return the user object or false if authentification failed
- * @warning this function enciphers the password
+ * List posts
+ * @return an array of the objects of each post
+ * @warning this function does not return the passwords
  */
-function check_auth($username, $password) {
-    return false;
+function get_all() {
+    return [get(1)];
 }
 
 /**
- * Follow another user
- * @param id the current user's id
- * @param id_to_follow the user's id to follow
- * @return true if the user has been followed, false else
+ * Get a post's likes
+ * @param pid the post's id
+ * @return the users objects who liked the post
  */
-function follow($id, $id_to_follow) {
-    return false;
+function get_likes($pid) {
+    return [\Model\User\get(2)];
 }
 
+/**
+ * Get a post's responses
+ * @param pid the post's id
+ * @return the posts objects which are a response to the actual post
+ */
+function get_responses($pid) {
+    return [get(2)];
+}
+
+/**
+ * Get stats from a post (number of responses and number of likes
+ */
+function get_stats($pid) {
+    return (object) array(
+        "nb_likes" => 10,
+        "nb_responses" => 40
+    );
+}
+
+/**
+ * Like a post
+ * @param uid the user's id to like the post
+ * @param pid the post's id to be liked
+ * @return true if the post has been liked, false else
+ */
+function like($uid, $pid) {
+    return false;
+}
