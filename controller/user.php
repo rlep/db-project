517768a25233bd2bc7cc07b2f02b1dcef5675fdb
Maxime Martineau <maxime@prafiny.org>
2016-12-03 21:23:54 +0100

    user controller structure

---
+++ controller/user.php
@@ -0,0 +1,26 @@
+<?php
+namespace Controller\User;
+
+function user_page() {
+
+}
+
+function signup_page() {
+
+}
+
+function signup() {
+
+}
+
+function login() {
+
+}
+
+function login_page() {
+
+}
+
+function logout() {
+
+}
