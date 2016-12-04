cb130d5850cf8d50e2327a607a8f832d36dc3cb5
Maxime Martineau <maxime@prafiny.org>
2016-12-04 22:11:10 +0100

    first commit for posts

--- controller/post.php
+++ controller/post.php
@@ -1,19 +1,66 @@
 <?php
 namespace Controller\Post;
 
-function post_page() {
+function post_page($id) {
+    $post = \Model\Post\get($id);
+    if(!$post) {
+        header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found", true, 404);
+        return;
+    }
+    require '../view/post.php';
+}
+
+function post($form) {
+    $user = \Session\get_user();
+    if(!$user) {
+        header($_SERVER["SERVER_PROTOCOL"]." 403 Forbidden", true, 403);
+        return;
+    }
+    extract($form);
+    if($pid = \Model\Post\create($user->id, $text)) {
+        \Session\set_success("Your twirp has been published.");
+        header("Location: post.php?id=".$pid);
+    }
+    else {
+        \Session\set_error("An error occured while trying to publish your twirp.");
+        header("Location: index.php");
+    }
+}
+
+function respond($id, $form) {
+    $post = \Model\Post\get($id);
+    if(!$post) {
+        header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found", true, 404);
+        return;
+    }
+    $user = \Session\get_user();
+    if(!$user) {
+        header($_SERVER["SERVER_PROTOCOL"]." 403 Forbidden", true, 403);
+        return;
+    }
+    extract($form);
+    if(\Model\Post\create($user->id, $text, $id)) {
+        \Session\set_success("Your response has been published.");
+        header("Location: post.php?id=".$id);
+    }
+    else {
+        \Session\set_error("An error occured while trying to publish your twirp.");
+        header("Location: post.php?id=".$id);        
+    }
 
 }
 
-function post() {
-
-}
-
-function respond() {
-
-}
-
-function destroy() {
+function destroy($id) {
+    $post = \Model\Post\get($id);
+    if(!$post) {
+        header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found", true, 404);
+        return;
+    }
+    $user = \Session\get_user();
+    if(!$user || $user->id !== $id) {
+        header($_SERVER["SERVER_PROTOCOL"]." 403 Forbidden", true, 403);
+        return;
+    }
 
 }
 
--- www/post.php
+++ www/post.php
@@ -1,4 +1,12 @@
 <?php
 require '../lib/main.php';
-Controller\Post\post_page();
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    Controller\Post\post($_POST, $_FILES);
+}
+elseif(isset($_GET['id'])) {
+    Controller\Post\post_page($_GET['id']);
+}
+else {
+    header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found", true, 404);
+}
 require '../lib/closure.php';
