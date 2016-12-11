70cca5f28ad6914e074834da7b540d041faf1663
Maxime Martineau <maxime@prafiny.org>
2016-12-11 17:44:23 +0100

    followed

--- controller/main.php
+++ controller/main.php
@@ -1,7 +1,22 @@
 <?php
 namespace Controller\Main;
 function main() {
-    $posts = \Model\Post\list_all("DESC");
+    $user = \Session\get_user();
+    if($user) {
+        $post = array();
+        $f = array_map(function($u){
+            return $u->id;
+        }, \Model\User\get_followings($user->id));
+        $p = \Model\Post\list_all("DESC");
+        foreach($p as $post) {
+            if(in_array($p->author->id, $f)) {
+                $posts[] = $p;
+            }
+        }
+    }
+    else {
+        $posts = \Model\Post\list_all("DESC");
+    }
     $popular_h = \Model\Hashtag\list_popular_hashtags(10);
     require "../view/timeline.php";
 }
